15661216dc2d17534983e2a73788051af2888bd0
maple <maple._wine@csc.jp>
2016-12-26 13:27:49 +0900

    add more functions but is not finishied yet.

--- genetic_algorithm/genetic_algo_final.cpp
+++ genetic_algorithm/genetic_algo_final.cpp
@@ -63,4 +63,108 @@ int main()
 		roulette_selection(num, length, population, fitness);
 	}
 
-	
+
+	for ( i=0; i<num; i++){
+		delete []population[i];
+		delete []pair[i];
+	}
+	delete []population;
+	delete []pair;
+	delete []fitness;
+
+	return 0;
+}
+
+int calc_fitness ( int individual, int length, int **population){
+	int i;
+	int fitness = 0;
+	for( i=0; i<length; i++){
+		fitness = fitness + population [individual][i];
+	}
+	return 10 * fitness;
+}
+
+void roulette_selection(int num, int length, int **population, int *fitness){
+	int i, j, k;
+	int sum_fitness;
+	int **new_population;
+	double *roulette;
+	double *ac_roulette;
+	double r;
+
+	new_population = new int * [num];
+	for( i=0; i<num; i++){
+		new_population[i] = new int [length];
+	}
+	roulette = new double[num];
+	ac_roulette = new double[num];
+
+	sum_fitness = 0;
+	for ( i=0; i< num; i++){
+		sum_fitness = sum_fitness + fitness [i];
+	}
+	roulette[0] = (double)fitness[0] / (double)sum_fitness;
+	ac_roulette[0] = roulette[0];
+
+	for( i=1; i<num; i++){
+		roulette[i] = (double)fitness[i] / (double) sum_fitness;
+		ac_roulette[i] = ac_roulette[i-1] + roulette[i];
+	}
+
+	for( i=0; i<num; i++){
+		r = (double)rand() / (double) RAND_MAX;
+		for( j=0; j<num; j++){
+			if( r<=ac_roulette[j]){
+				for (k=0; k<length; k++){
+					new_population[i][k] = population[j][k];
+				}
+				break;
+			}
+		}
+	}
+
+	for( i=0; i<num; i++) {
+		for ( k=0; k<length; k++){
+			population[i][k] = new_population[i][k];
+		}
+	}
+
+	delete []roulette;
+	delete []ac_roulette;
+
+	for( i=0; i<num; i++){
+		delete []new_population[i];
+	}
+	delete []new_population;
+
+}
+
+void one_point_crossover(int crossover_rate, int mutation_rate, int num, int length, int **population, int **pair){
+	int i, j;
+	int cross_point;
+
+	for ( i=0; i<(num-1) ; i=i+2 ){
+		if(rand() % 100<crossover_rate){
+			cross_point = rand()%(length-1) + 1;
+			// printf(*cross_point = %d \n", cross_point);
+			for(j=0; j<cross_point; j++){
+				population[i][j] = pair[i+1][j];
+				population[i+1][j] = pair[i][j];
+			}
+			for( j=cross_point; j<length; j++){
+				population[i][j] = pair[i][j];
+				population[i+1][j] = pair[i+1][j];
+			}
+
+			mutation(mutation_rate, length, i, population);
+			mutation(mutation_rate, length, i+1, population);
+		}else {
+			// do NOT apply cross_point
+			for( j=0; j<length; j++){
+				population[i][j] = pair[i][j];
+				population[i+1][j] = pair[i+1][j];
+			}
+		}
+	}
+}
+
